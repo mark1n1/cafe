c8aed8c4ff7e39765fb1144c801cd98c718f6deb
Marcos Valderrabano <ing.marco.valu@gmail.com>
2024-01-08 10:44:55 -0800

    added images and pkg json

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
+
+//CSS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+//Images
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     //compile sass
@@ -16,9 +23,39 @@ function css(done) {
     done();
 }
 
+function images() {
+    return src('src/img/**/*')
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest('build/img'));
+}
+
+function versionWebp() {
+    //optional to reduce quality of images and save some space
+    /*
+    const options = {
+        quality: 50
+    }
+    */
+    return src('src/img/**/*.{png,jpg}')
+            .pipe(webp())
+            .pipe(dest('build/img'));
+}
+
+function versionAvif() {
+    return src('src/img/**/*.{png,jpg}')
+            .pipe(avif())
+            .pipe(dest('build/img'));
+}
+
 function dev() {
-    watch('src/scss/app.scss', css);
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', images);
+    // watch('src/scss/app.scss', css);
 }
 
 exports.css = css;
 exports.dev = dev;
+exports.images = images;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series(images, versionWebp, versionAvif, css, dev);
